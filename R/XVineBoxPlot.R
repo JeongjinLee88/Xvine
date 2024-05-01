f93eadf7c08300eeb3857f7afac4eefa8c2bab0b
JeongjinLee88 <jeongjinlee88@gmail.com>
2024-05-01 16:32:57 +0200

    updates from Rstudio

--- R/XVineBoxPlot.R
+++ R/XVineBoxPlot.R
@@ -53,7 +53,8 @@
 #'                    0, 0, 0, 0, 0),5,byrow = TRUE)
 #' # X-vine specification
 #' XVS=XVineSpec(M = StrMtx, Mmod = FamMtx, Mpar = ParMtx)
-#' BoxOut2K05=XVineBoxplot(N = 2000, qt = 0.05, ite = ite, XVS = XVS, RankT = TRUE)
+#' # To run, uncomment it.
+#' # XVineBoxplot(N = 500, qt = 0.05, ite = ite, XVS = XVS, RankT = TRUE)
 XVineBoxplot <- function(N, qt, ite, XVS, RankT=TRUE, familyset_tc=c(1:4), familyset_cop=c(0,1,3,4,5,6,13,14,16)){
   
   #  Matrices to be stored
@@ -63,9 +64,7 @@ XVineBoxplot <- function(N, qt, ite, XVS, RankT=TRUE, familyset_tc=c(1:4), famil
   MLE2Dep_WithoutFamSel <- array(NA,dim=c(d,d,ite)) # matrices of dep measures without family selections
   MLE2Dep_WithFamSel <- array(NA,dim=c(d,d,ite)) # matrices of dep measures with family selections
   FamMtxXVS=XVS$fmat[,,1]
-  #DepMtx_Cop <- array(NA,dim=c(d,d,ite)) # matrices of parameter estimates
-  #AvgEstMtx <- matrix(NA,d,d) # average matrix
-  #AvgEstMtx=apply(EstMtx,1:2,mean) # sample average
+  Var1<-Var2<-value<-NULL
   
   #  Repeat simulation 'ite' times
   if(RankT){
@@ -102,7 +101,6 @@ XVineBoxplot <- function(N, qt, ite, XVS, RankT=TRUE, familyset_tc=c(1:4), famil
   all.pairs <- combn(1:d, 2)
   MLest4eachpair=apply(all.pairs, 2, function(ind) rbind(MLE_WithoutFamSel[ind[1],ind[2],]))  
   data_longformat=melt(MLest4eachpair)
-  data_longformat$Var2 <- as.factor(data_longformat$Var2)
   for(i in 1:(d-1)){
     a=cumsum((d-1):1)
     b=cumsum((d-1):1)+1
@@ -113,43 +111,14 @@ XVineBoxplot <- function(N, qt, ite, XVS, RankT=TRUE, familyset_tc=c(1:4), famil
     }
   }
   data_longformat$Var1 <- as.character(data_longformat$Var1)
-  TrueMtx=t(XVS$pmat[,,1]) # Specified MLEs
-  y0s <- c(TrueMtx[lower.tri(TrueMtx)])
-  dLines <- data.frame(X =1:choose(d,2) - 0.4,
-                       Y = y0s,
-                       Xend = 1:choose(d,2) + 0.4,
-                       Yend = y0s,
-                       Group = c("typeA"),
-                       color = c("red"))
+  data_longformat$Var2 <- as.factor(data_longformat$Var2)
   bp_mle_SPE <- ggplot(data = data_longformat,aes(x=Var2,y=value,fill=Var1)) +
-    geom_boxplot() +
-    labs(title="",x="", y = "") +
-    scale_fill_manual(labels=c(expression(T[1]),expression(T[2]),expression(T[3]),expression(T[4])),values=c("lightskyblue","lightgreen","lavender","papayawhip")) + 
-    # legend labels
-    theme(legend.title = element_blank(),
-          legend.text = element_text(face="bold", size=15),
-          legend.key.size = unit(1, 'cm'),
-          legend.position="none",
-          aspect.ratio = 1,
-          axis.text.x = element_text(color="black", face="bold", size=9, angle = 20, hjust = 0.5),
-          axis.text.y = element_text(face="bold", size=9),
-          panel.background = element_rect(fill = "white",
-                                          colour = "white",
-                                          linewidth = 0.5, linetype = "solid"),
-          panel.grid.major = element_line(linewidth = 0.5, linetype = 'solid',
-                                          colour = "grey"), 
-          panel.grid.minor = element_line(linewidth = 0.25, linetype = 'solid',
-                                          colour = "grey"),
-          plot.margin = margin(0, 1, 0, 0, "cm")) +
-    scale_y_continuous(breaks = seq(-1,4,1),limits = c(-1,4,1)) +
-    geom_segment(data = dLines, color = "red",aes(x = X, y = Y, xend = Xend, yend=Yend), inherit.aes = FALSE) +
-    scale_x_discrete(labels=c(expression(atop(chi[12],"\n(HR)")),expression(atop(chi[23],"\n(NL)")),expression(atop(chi[24],"\n(L)")),expression(atop(chi[45],"\n(Diri)")),expression(atop(tau[13~";"~2],"\n(Clay)")),expression(atop(tau[34~";"~2],"\n(Gum)")),expression(atop(tau[25~";"~4],"\n(Ga)")),expression(atop(tau[14~";"~23],"\n(Clay)")),expression(atop(tau[35~";"~24],"\n(Ga)")),expression(atop(tau[15~";"~234],"\n(Ga)"))))
-  
+                geom_boxplot() +
+                labs(title="",x="", y = "")
   ## 2. MLE after selecting bivariate parametric families
   all.pairs <- combn(1:d, 2)
   MLest4eachpair=apply(all.pairs, 2, function(ind) rbind(MLE_WithFamSel[ind[1],ind[2],]))
   data_longformat=melt(MLest4eachpair)
-  data_longformat$Var2 <- as.factor(data_longformat$Var2)
   for(i in 1:(d-1)){
     a=cumsum((d-1):1)
     b=cumsum((d-1):1)+1
@@ -160,46 +129,15 @@ XVineBoxplot <- function(N, qt, ite, XVS, RankT=TRUE, familyset_tc=c(1:4), famil
     }
   }
   data_longformat$Var1 <- as.character(data_longformat$Var1)
-  TrueMtx=t(XVS$pmat[,,1]) # Specified MLEs
-  y0s <- c(TrueMtx[lower.tri(TrueMtx)])
-  dLines <- data.frame(X =1:choose(d,2) - 0.4,
-                       Y = y0s,
-                       Xend = 1:choose(d,2) + 0.4,
-                       Yend = y0s,
-                       Group = c("typeA"),
-                       color = c("red"))
+  data_longformat$Var2 <- as.factor(data_longformat$Var2)
   bp_mle_Fam <- ggplot(data = data_longformat,aes(x=Var2,y=value,fill=Var1)) +
-    geom_boxplot() +
-    labs(title="",x="", y = "") +
-    scale_fill_manual(labels=c(expression(T[1]),expression(T[2]),expression(T[3]),expression(T[4])),values=c("lightskyblue","lightgreen","lavender","papayawhip")) + 
-    # legend labels
-    theme(legend.title = element_blank(),
-          legend.text = element_text(face="bold", size=15),
-          legend.key.size = unit(1, 'cm'),
-          legend.position="none",
-          aspect.ratio = 1,
-          axis.text.x = element_text(color="black", face="bold", size=9, angle = 20, hjust = 0.5 ),
-          axis.text.y = element_text(face="bold", size=9),
-          panel.background = element_rect(fill = "white",
-                                          colour = "white",
-                                          linewidth = 0.5, linetype = "solid"),
-          panel.grid.major = element_line(linewidth = 0.5, linetype = 'solid',
-                                          colour = "grey"), 
-          panel.grid.minor = element_line(linewidth = 0.25, linetype = 'solid',
-                                          colour = "grey"),
-          plot.margin = margin(0, 1, 0, 0, "cm")) +
-    scale_y_continuous(breaks = seq(-4,8.5,1.5),limits = c(-4,8.5,1.5)) +
-    geom_segment(data = dLines, color = "red",aes(x = X, y = Y, xend = Xend, yend=Yend), inherit.aes = FALSE) +
-    scale_x_discrete(labels=c(expression(theta[12]),expression(theta[23]),expression(theta[24]),expression(theta[45]),
-                              expression(theta[13~";"~2]),expression(theta[34~";"~2]),expression(theta[25~";"~4]),expression(theta[14~";"~23]),
-                              expression(theta[35~";"~24]),expression(theta[15~";"~234])))
-  
+                geom_boxplot() +
+                labs(title="",x="", y = "")
   
   ##  3. Dependence measures through SPE
   all.pairs <- combn(1:d, 2)
   MLest4eachpair=apply(all.pairs, 2, function(ind) rbind(MLE2Dep_WithoutFamSel[ind[1],ind[2],]))
   data_longformat=melt(MLest4eachpair)
-  data_longformat$Var2 <- as.factor(data_longformat$Var2)
   for(i in 1:(d-1)){
     a=cumsum((d-1):1)
     b=cumsum((d-1):1)+1
@@ -210,43 +148,14 @@ XVineBoxplot <- function(N, qt, ite, XVS, RankT=TRUE, familyset_tc=c(1:4), famil
     }
   }
   data_longformat$Var1 <- as.character(data_longformat$Var1)
-  TrueMtx_dep=t(DependMtx(XVS)) # Dependence measures
-  y0s <- c(TrueMtx_dep[lower.tri(TrueMtx_dep)])
-  dLines <- data.frame(X =1:choose(d,2) - 0.4,
-                       Y = y0s,
-                       Xend = 1:choose(d,2) + 0.4,
-                       Yend = y0s,
-                       Group = c("typeA"),
-                       color = c("red"))
+  data_longformat$Var2 <- as.factor(data_longformat$Var2)
   bp_dep_SPE <- ggplot(data = data_longformat,aes(x=Var2,y=value,fill=Var1)) +
-    geom_boxplot() +
-    labs(title="",x="", y = "") +
-    scale_fill_manual(labels=c(expression(T[1]),expression(T[2]),expression(T[3]),expression(T[4])),values=c("lightskyblue","lightgreen","lavender","papayawhip")) + 
-    # legend labels
-    theme(legend.title = element_blank(),
-          legend.text = element_text(face="bold",size=15),
-          legend.key.size = unit(1, 'cm'),
-          legend.position="none",
-          aspect.ratio = 1,
-          axis.text.x = element_text(color="black", face="bold", size=9, angle = 20, hjust = 0.5 ),
-          axis.text.y = element_text(face="bold", size=9),
-          panel.background = element_rect(fill = "white",
-                                          colour = "white",
-                                          linewidth = 0.5, linetype = "solid"),
-          panel.grid.major = element_line(linewidth = 0.5, linetype = 'solid',
-                                          colour = "grey"), 
-          panel.grid.minor = element_line(linewidth = 0.25, linetype = 'solid',
-                                          colour = "grey"),
-          plot.margin = margin(0, 1, 0, 0, "cm")) +
-    scale_x_discrete(labels=c(expression(atop(chi[12],"\n(HR)")),expression(atop(chi[23],"\n(NL)")),expression(atop(chi[24],"\n(L)")),expression(atop(chi[45],"\n(Diri)")),expression(atop(tau[13~";"~2],"\n(Clay)")),expression(atop(tau[34~";"~2],"\n(Gum)")),expression(atop(tau[25~";"~4],"\n(Ga)")),expression(atop(tau[14~";"~23],"\n(Clay)")),expression(atop(tau[35~";"~24],"\n(Ga)")),expression(atop(tau[15~";"~234],"\n(Ga)")))) +
-    scale_y_continuous(breaks = seq(-0.4,0.8,0.2),limits = c(-0.4,0.8)) +
-    geom_segment(data = dLines, color = "red",aes(x = X, y = Y, xend = Xend, yend=Yend), inherit.aes = FALSE)
-  
+                geom_boxplot() +
+                labs(title="",x="", y = "")
   ##  4. Dependence measures after selecting bivariate parameteric families
   all.pairs <- combn(1:d, 2)
   MLest4eachpair=apply(all.pairs, 2, function(ind) rbind(MLE2Dep_WithFamSel[ind[1],ind[2],]))
   data_longformat=melt(MLest4eachpair)
-  data_longformat$Var2 <- as.factor(data_longformat$Var2)
   for(i in 1:(d-1)){
     a=cumsum((d-1):1)
     b=cumsum((d-1):1)+1
@@ -257,40 +166,9 @@ XVineBoxplot <- function(N, qt, ite, XVS, RankT=TRUE, familyset_tc=c(1:4), famil
     }
   }
   data_longformat$Var1 <- as.character(data_longformat$Var1)
-  TrueMtx_dep=t(DependMtx(XVS)) # Dependence measures
-  y0s <- c(TrueMtx_dep[lower.tri(TrueMtx_dep)])
-  dLines <- data.frame(X =1:choose(d,2) - 0.4,
-                       Y = y0s,
-                       Xend = 1:choose(d,2) + 0.4,
-                       Yend = y0s,
-                       Group = c("typeA"),
-                       color = c("red"))
+  data_longformat$Var2 <- as.factor(data_longformat$Var2)
   bp_dep_Fam <- ggplot(data = data_longformat,aes(x=Var2,y=value,fill=Var1)) +
-    geom_boxplot() +
-    labs(title="",x="", y = "") +
-    scale_fill_manual(labels=c(expression(T[1]),expression(T[2]),expression(T[3]),expression(T[4])),values=c("lightskyblue","lightgreen","lavender","papayawhip")) + 
-    # legend labels
-    theme(legend.title = element_blank(),
-          legend.text = element_text(face="bold",size=15),
-          legend.key.size = unit(1, 'cm'),
-          legend.position="none",
-          aspect.ratio = 1,
-          axis.text.x = element_text(color="black", face="bold", size=9, angle = 20, hjust = 0.5 ),
-          axis.text.y = element_text(face="bold", size=9),
-          panel.background = element_rect(fill = "white",
-                                          colour = "white",
-                                          linewidth = 0.5, linetype = "solid"),
-          panel.grid.major = element_line(linewidth = 0.5, linetype = 'solid',
-                                          colour = "grey"), 
-          panel.grid.minor = element_line(linewidth = 0.25, linetype = 'solid',
-                                          colour = "grey"),
-          plot.margin = margin(0, 1, 0, 0, "cm")) +
-    scale_x_discrete(labels=c(expression(chi[12]),expression(chi[23]),expression(chi[24]),expression(chi[45]),
-                              expression(tau[13~";"~2]),expression(tau[34~";"~2]),expression(tau[25~";"~4]),expression(tau[14~";"~23]),
-                              expression(tau[35~";"~24]),expression(tau[15~";"~234]))) +
-    scale_y_continuous(breaks = seq(-0.4,0.8,0.2),limits = c(-0.4,0.8)) +
-    geom_segment(data = dLines, color = "red",aes(x = X, y = Y, xend = Xend, yend=Yend), inherit.aes = FALSE)
-  
-  
+                geom_boxplot() +
+                labs(title="",x="", y = "")
   return(list("MLE_WithoutFamSel"=MLE_WithoutFamSel,"MLE_WithFamSel"=MLE_WithFamSel,"MLE2Dep_WithoutFamSel"=MLE2Dep_WithoutFamSel,"MLE2Dep_WithFamSel"=MLE2Dep_WithFamSel,'bp_mle_SPE'=bp_mle_SPE, 'bp_mle_Fam'=bp_mle_Fam, 'bp_dep_SPE'=bp_dep_SPE, 'bp_dep_Fam'=bp_dep_Fam))
 }
---
+++ R/XVineFamSelSimul.R
@@ -0,0 +1,98 @@
+#' Simulation for the selection of bivariate parametric families
+#'
+#' @description
+#' `XVineFamSelSimul` returns simulation results of selecting bivariate parametric families
+#' by calling the function [XVineFamSel()].
+#' All information about the simulation result is saved in the list of `FamSelOut`. 
+#'  
+#' @param N Numeric; the sample size.
+#' @param Rank Logical; whether rank transformation is performed or not (\code{Rank=T}; default).
+#' @param qt Numeric; a lower enough threshold used in the rank transformation (e.g. switches from Pareto scale to uniform scale)
+#' @param ite Numeric; the number of iterations for simulation.
+#' @param XVS A list consisting of three components: reconstructed structure matrices, family matrices, parameter matrices, see:[XVineSpec()].
+#' @param familyset_tc Numeric vector; the model type of bivariate tail copulas.
+#' Possible tail copula models are:
+#' * 1=Husler-Reiss model
+#' * 2=Negative logistic model
+#' * 3=Logistic model
+#' * 4=Dirichlet model
+#' @param familyset_cop Numeric vector; the class of bivariate copula families with a single parameter.
+#' @param selectioncrit Character; the information criteria for model selections (default: `AIC`).
+#' @param effsampsize Integer; indicates the effective sample size (Default: 10) for the independence copula.
+#' @param tau_threshold Numeric; indicates the value of the Kendall's tau (Default: 0.05) for the independence copula.
+#' @param edgeIndex Numeric vector; the index set that indicates the location of the matrix for the corresponding edge.
+#'
+#' @return 
+#' 1. `FamSelOut`: A list of \eqn{d\times d} strict upper triangular matrices from the function [XVineFamSel()] for iterations.
+#' 2. `EffSizePercent`: A \eqn{d\times d} strict upper triangular matrix including the percentages of the effective sample size.
+#' 3. `GlobalProp`: Numeric; the proportion of correctly selected families over all trees.
+#' 4. `TreeProp`: Numeric vector; the proportion of correctly selected families for each level tree.
+#' 5. `EdgeProp`: A \eqn{d\times d} strict upper triangular matrix including the proportion of correctly selected families for each edge in each tree.
+#' 6. `SelectIndCop`: Numeric; the number of times the algorithm chooses the independence copula for the specified edge by `edgeIndex`.
+#' @export
+#'
+#' @examples
+#' StrMtx <- matrix(c(1, 1, 2, 2, 4,
+#' 0, 2, 1, 3, 2,
+#' 0, 0, 3, 1, 3,
+#' 0, 0, 0, 4, 1,
+#' 0, 0, 0, 0, 5),5,byrow = TRUE)
+#' ParMtx <- matrix(c(0, 1.5, 2, 2.5, 2,
+#'                 0, 0, 2, 2.5, 0.7,
+#'                 0, 0, 0, 0.4, -0.3,
+#'                 0, 0, 0, 0, 0.1,
+#'                 0, 0, 0, 0, 0),5,byrow = TRUE)
+#' FamMtx <- matrix(c(0, 1, 2, 3, 4,
+#'                    0, 0, 3, 4, 1,
+#'                    0, 0, 0, 3, 1,
+#'                    0, 0, 0, 0, 1,
+#'                    0, 0, 0, 0, 0),5,byrow = TRUE)
+#' # X-vine specification
+#' XVS=XVineSpec(M = StrMtx, Mmod = FamMtx, Mpar = ParMtx)
+#' XVineFamSelSimul(N = 1000, qt = 0.05, ite = 200, XVS = XVS
+#' ,edgeIndex = c(4,5), familyset_tc=c(1:4), familyset_cop = c(0,1,3,4,5,6,13,14,16))
+XVineFamSelSimul <- function(N, Rank=TRUE, qt, ite, XVS, edgeIndex, familyset_tc, familyset_cop
+                             , selectioncrit = 'AIC', effsampsize = 10, tau_threshold = 0.05)
+{
+  d <- dim(XVS$xmat)[1]
+  EdgeTotal=sum(1:(d-1))
+  FamSelOut <- list()
+  
+  ##  Repeat generating Pareto samples and performing family selections
+  for(i in 1:ite){
+    Dat_P=ParetoSim(n = N, XVS = XVS)
+    FamSelOut[[i]] <- XVineFamSel(data = Dat_P, Rank = Rank, qt = qt, XVS = XVS, famset_tc = familyset_tc, famset_cop = familyset_cop
+                                  , selectioncrit = selectioncrit,effsampsize = effsampsize, tau_threshold = tau_threshold)
+  }
+  
+  ##  Extract the effective sample size matrices
+  EffSizeComb=lapply(1:ite, function(i)FamSelOut[[i]]$EffectSamp)
+  EffSizeComb=array(unlist(EffSizeComb),dim=c(d,d,ite))
+  ##  Convert to the percentage of effective sample size with respect to the sample size N
+  Avg.EffectSize=apply(X = EffSizeComb, c(1,2), function(x) mean(x,na.rm=T))
+  EffSizePercent=(Avg.EffectSize/N)*100 # percentages of effective sample size
+  #mean(EffSizeComb[3,4,] < 20) # percentage of effective sample size being less than a certain cutoff over iterations
+  
+  ####  Proportion of correctly selected families
+  
+  ##  1. Proportion of correctly selected families over all trees
+  CountComb=lapply(1:ite,function(i)FamSelOut[[i]]$count)
+  CountComb=array(unlist(CountComb),dim=c(dim(XVS$xmat)[1],dim(XVS$xmat)[1],ite))
+  GlobalProp=sum(apply(X = CountComb,c(1,2),function(x)sum(x)),na.rm = T)/(EdgeTotal*ite)
+  
+  ##  2. The overall proportion of correctly selected families for each tree level
+  Num=((d-1):1)*(ite)
+  TreeProp=rowSums(apply(X = CountComb,c(1,2),function(x)sum(x)),na.rm=T)[1:(d-1)]/Num
+  
+  ##  3. Proportion of correctly selected families for each edge in each tree
+  EdgeProp=apply(X = CountComb, c(1,2), function(x) mean(x,na.rm=T))  
+  
+  ##  4. How many times the algorithm selects the independence copula for a specific edge
+  FamComb=lapply(1:ite,function(i)FamSelOut[[i]]$famsel)
+  FamComb=array(unlist(FamComb),dim=c(d,d,ite))
+  SelectIndCop=sum(FamComb[edgeIndex[1],edgeIndex[2],]==0)/ite 
+  # if edge_index=(4,5), how many times the algorithm chooses the independence copula for the deepest edge.
+  
+  return(list("FamSelOut"=FamSelOut,"EffSizePercent"=EffSizePercent,"GlobalProp"=GlobalProp,"TreeProp"=TreeProp,"EdgeProp"=EdgeProp,"SelectIndCop"=SelectIndCop))
+}
+
